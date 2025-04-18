07cbf2c1e0ee7eca16faff70c7492ac0c408343e
PoojaryKeerthan <keerthanpoojary221@gmail.com>
2025-04-18 11:28:47 +0530

    cors issue corrected1

--- Server/index.js
+++ Server/index.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(cors({
   origin: [
     "http://localhost:5173", // for local frontend
-    "https://find-it-olive.vercel.app/"
+    "https://find-it-olive.vercel.app"
   ],
   credentials: true,
 }));
